eb7322618c405ea6fc8f0922efc0d6c42d634269
AlaminDolar <amin15-9898@diu.edu.bd>
2022-03-30 12:45:26 +0600

    use direct function

--- js/event.js
+++ js/event.js
@@ -1,9 +1,25 @@
+
+
+
+function makegreen() {
+    document.body.style.backgroundColor = "red";
+  }
+
+
+
+
 const blueButton = document.getElementById('bluebutton');
 blueButton.onclick=makeB;
-
-
 
 function makeB(){
 
     document.body.style.backgroundColor='blue';
 }
+
+
+//use direct function
+
+const green = document.getElementById("make-green");
+
+green.onclick=function(){
+  document.body.style.backgroundColor="green";}
